f752cd620fcd14678a7a2a071f264c18e72480d7
etiennegodin <etiennegodin@duck.com>
2026-03-10 11:24:59 -0400

    moved sample strat to api

--- src/inat_pipeline/workflows/ingest_api_workflow.py
+++ src/inat_pipeline/workflows/ingest_api_workflow.py
@@ -2,6 +2,7 @@ import logging
 from typing import Union
 
 from ..app.container import Dependencies
+from ..utils.db import SQL_Engine, _open_connection
 from . import (
     ingest_api_observations_workflow,
     ingest_api_taxa_workflow,
@@ -11,6 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 def execute(deps: Dependencies, api_limit: Union[None, int]):
+    logger.info("Running sample strategy to select observations to query")
+    con = _open_connection(deps.DB_PATH)
+    sql = SQL_Engine(con, deps.SQL_API_PATH)
+    sql.execute("sample")
+
     logger.info("Starting api observations workflow")
     ingest_api_observations_workflow.execute(deps, limit=api_limit)
 
--- src/inat_pipeline/workflows/ingest_local_workflow.py
+++ src/inat_pipeline/workflows/ingest_local_workflow.py
@@ -2,7 +2,7 @@ import logging
 
 from ..app.container import Dependencies
 from ..ingest.downloads import ingest_downloads
-from ..utils.db import SQL_Engine, _open_connection
+from ..utils.db import _open_connection
 
 logger = logging.getLogger(__name__)
 
@@ -11,18 +11,17 @@ def execute(deps: Dependencies):
     con = _open_connection(deps.DB_PATH)
     con.execute("CREATE SCHEMA IF NOT EXISTS raw")
 
-    # Ingest csv files to db
+    # Ingest observations csv files
+    source = "downloads"
     ingested = ingest_downloads(con, "downloads", deps.DOWNLOADS_FOLDER)
-    logger.info(f"Ingested {len(ingested)} files ")
+    logger.info(f"Ingested {len(ingested)} files from raw/{source}")
 
     # Ingest taxa
-    ingested = ingest_downloads(con, "taxa", deps._RAW_DATA_FOLDER / "taxa")
-    logger.info(f"Ingested {len(ingested)} files ")
+    source = "taxa"
+    ingested = ingest_downloads(con, source, deps._RAW_DATA_FOLDER / source)
+    logger.info(f"Ingested {len(ingested)} files raw/{source} ")
 
     # Ingest places
-    ingested = ingest_downloads(con, "places", deps._RAW_DATA_FOLDER / "places")
-    logger.info(f"Ingested {len(ingested)} files ")
-
-    # Select observations to sample
-    sql = SQL_Engine(con, deps.SQL_API_PATH)
-    sql.execute("sample")
+    source = "places"
+    ingested = ingest_downloads(con, source, deps._RAW_DATA_FOLDER / source)
+    logger.info(f"Ingested {len(ingested)} files raw/{source}")
