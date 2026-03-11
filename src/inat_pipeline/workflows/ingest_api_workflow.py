7915aa8106f3aa644567804025d910b7e7421f71
etiennegodin <etiennegodin@duck.com>
2026-03-11 16:48:54 -0400

    removed stage sql after inat api

--- src/inat_pipeline/workflows/ingest_api_workflow.py
+++ src/inat_pipeline/workflows/ingest_api_workflow.py
@@ -1,18 +1,12 @@
 import logging
 
 from ..app.container import Dependencies
-from ..db import DuckDBConnection, SQLEngine
 from . import ingest_api_observations_workflow, ingest_api_similar_species_workflow
 
 logger = logging.getLogger(__name__)
 
 
 def execute(deps: Dependencies, rate: int, ignore_not_found: bool):
-    logger.info("Running sample strategy to select observations to query")
-    con = DuckDBConnection(deps.DB_PATH)
-    sql = SQLEngine(con, deps.SQL_API_PATH)
-    sql.execute("sample")
-
     logger.info("Starting api observations workflow")
     ingest_api_observations_workflow.execute(
         deps, rate=rate, ignore_not_found=ignore_not_found
