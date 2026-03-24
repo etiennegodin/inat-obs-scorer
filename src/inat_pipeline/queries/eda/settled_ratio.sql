d02bfdf9b9ab9ef0f51dc1e6e4b2c53cc408dfcf
etiennegodin <etiennegodin@duck.com>
2026-03-24 17:38:10 -0400

    sqlfluff

--- src/inat_pipeline/queries/eda/settled_ratio.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-AVG(cumulative_settled_events / cumulative_events) as ratio_mean,
-STDDEV(cumulative_settled_events / cumulative_events) as ratio_std
-FROM graph.user_role_timeline
--- src/inat_pipeline/queries/features/label.sql
+++ src/inat_pipeline/queries/features/label.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE TABLE features.label AS
 
-SELECT *EXCLUDE(is_rg),
-is_rg AS label
+--test
+SELECT
+    * EXCLUDE (is_rg),
+    is_rg AS label
 FROM research_grade_windowed(to_days(:label_window_days))
