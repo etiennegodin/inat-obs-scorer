644b1ac4fcc921c1d2e47023cd67966a6954214a
etiennegodin <etiennegodin@duck.com>
2026-04-24 13:47:05 -0400

    fix on staging new photo fields

--- src/inat_pipeline/queries/stage/stage_obs_photos.sql
+++ src/inat_pipeline/queries/stage/stage_obs_photos.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE TABLE staged.photos AS
 SELECT
     o.id AS observation_id,
-    UNNEST(o.observation_photos, RECURSIVE := true)
+    UNNEST(o.photos, RECURSIVE := true)
 FROM staged.observations o;
 ALTER TABLE staged.photos
 RENAME id TO photo_id;
